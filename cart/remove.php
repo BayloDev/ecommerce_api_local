7b382ecc5aff82c83d28e30af056cf0cddefcd44
Bilal@Dev <baylo1557@gmail.com>
2024-07-23 18:20:30 +0100

    23 jul

--- cart/remove.php
+++ cart/remove.php
@@ -1,11 +1,6 @@
 <?php
 include '../connect.php';
 $cart_ids = filterRequest('cart_ids');
-//print_r($cart_ids);
-//$data = array(5834,5,654);
-//print_r($data) ;
-//$fields = implode(',', array_values($data));
-//echo $fields;
 $stmt = $con->prepare("DELETE FROM cart WHERE cart_id IN ($cart_ids) ");
 $stmt->execute();
 $count = $stmt->rowCount();
